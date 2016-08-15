687ee3106e35f1de729698769c76317f5a219336
Joel Klaus <jaklaus0101@gmail.com>
2016-08-15 15:15:43 -0500

    finish slider functions

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,60 @@
 	var sliderInt = 1;
 	var sliderNext = 2;
+	var count = $('img').length
 
 $('document').ready(function(){
 
 	fadeInImage();
 	startSlider();
+	nextButton();
+	backButton();
 });
 
 function startSlider(){
-	var count = $('img').length
 	var loop = setInterval(function(){
+		if(sliderInt === count)
+			sliderInt = 0;
 
-		$('img').fadeOut(1000);
-		$('img#' + sliderNext).fadeIn(1000);
+		sliderInt += 1
 
-		sliderInt = sliderNext
-		sliderNext += 1;
-		
-		if(sliderInt === count){
-			sliderNext = 1;
-		}
+		$('img').fadeOut(300);
+		$('img#' + sliderInt).fadeIn(300);
 
-	}, 3000)
+
+
+	}, 5000)
 };
 
 function fadeInImage(){
 		$("img#" + sliderInt).fadeIn(1000);
 	}
+
+function nextButton(){
+	$('#next').click(function(){
+		sliderInt += 1;
+		fadeToNext(sliderInt);
+	})
+}
+
+function backButton(){
+	$('#back').click(function(){
+		sliderInt -= 1;
+		fadeToNext(sliderInt);
+		
+	})
+}
+
+function fadeToNext(id){
+	if(id > count){
+		id = 1;
+		sliderInt = 1;
+	}
+
+	if(id < 1){
+		id = count;
+		sliderInt = count;
+	}
+	$('img').fadeOut(1000);
+	$('img#' + id).fadeIn(1000);
+	
+}
