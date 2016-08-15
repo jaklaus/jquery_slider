292637288d547bd674010f43cb2b9ff927a131ba
Joel Klaus <jaklaus0101@gmail.com>
2016-08-15 14:00:24 -0500

    cyle through slides

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,29 @@
-var sliderInt = 1;
-var sliderNext = 2;
+	var sliderInt = 1;
+	var sliderNext = 2;
 
 $('document').ready(function(){
-	$('img#1').fadeIn(300);
+
+	fadeInImage();
+	startSlider();
 });
+
+function startSlider(){
+	var count = $('img').length
+	var loop = setInterval(function(){
+
+		$('img').fadeOut(1000);
+		$('img#' + sliderNext).fadeIn(1000);
+
+		sliderInt = sliderNext
+		sliderNext += 1;
+		
+		if(sliderInt === count){
+			sliderNext = 1;
+		}
+
+	}, 3000)
+};
+
+function fadeInImage(){
+		$("img#" + sliderInt).fadeIn(1000);
+	}
